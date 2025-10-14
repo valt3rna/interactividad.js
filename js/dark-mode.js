94c8ee310ee0a7d92689818b8ca4e388b458e091
Valt3rna <valerodriguezv7@gmail.com>
2025-10-13 18:56:52 -0600

    dark mode pantalla

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,14 @@
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+const colorPicker = document.getElementById("color-picker");
+const bgPicker = document.getElementById("bg-picker");
 /* themeToggleBtn.classList.add("ejemplo");
 */
 themeToggleBtn.addEventListener("click", function () {
     document.body.classList.toggle ("dark");
+});
+colorPicker.addEventListener("change", function () {
+    document.body.style.color = colorPicker.value;
+});
+bgPicker.addEventListener("change", function () {
+    document.body.style.backgroundColor = bgPicker.value;
 });
