c3cfc4656587454f1218848a77992fdc32dcdb05
Valt3rna <valerodriguezv7@gmail.com>
2025-10-13 18:39:21 -0600

    agregué dark mode

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,1 +1,6 @@
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
+/* themeToggleBtn.classList.add("ejemplo");
+*/
+themeToggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle ("dark");
+});
