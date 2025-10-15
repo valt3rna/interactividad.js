0a9ff227631c50064366690acbcae06e897a082c
Valt3rna <valerodriguezv7@gmail.com>
2025-10-15 09:18:21 -0600

    agregamos if/else

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -3,9 +3,16 @@ const colorPicker = document.getElementById("color-picker");
 const bgPicker = document.getElementById("bg-picker");
 /* themeToggleBtn.classList.add("ejemplo");
 */
+
 themeToggleBtn.addEventListener("click", function () {
     document.body.classList.toggle ("dark");
-});
+    if (document.body.classList.contains  ("dark")) {
+        themeToggleBtn.innerText = "Light Mode";
+    } else {
+        themeToggleBtn.innerText = "Dark Mode";   
+    }
+    });
+
 colorPicker.addEventListener("change", function () {
     document.body.style.color = colorPicker.value;
 });
